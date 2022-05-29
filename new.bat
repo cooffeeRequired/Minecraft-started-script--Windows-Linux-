f76f2ab9131ccbf9b7e61bd935dfaeb311c7aa40
cooffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2022-05-29 03:12:30 +0200

    Don't use it ... not finished version -- Progress 95%

--- new.bat
+++ new.bat
@@ -30,22 +30,31 @@ for /f "tokens=1* delims=: " %%A in (
 @rem ----------------------------------
 
 
-@rem ------ Variables ------------------
+@rem ------------Variables ------------------
 set properties=server.properties
-set tmp_ip=%temp%\Uu2zy_result.tmp && set tmp_port=%temp%\Uu2zy_result2.tmp
-set /p "ip_unformatted="<%tmp_ip% && set /p "port_unformatted="<%tmp_port%
-set ip=%ip_unformatted:~10,25% && set port=%port_unformatted:~12,25%
+set tmp_ip=%temp%\Uu2zy_result.tmp
+set tmp_port=%temp%\Uu2zy_result2.tmp
 
-@rem ---------- functions ----------------
-findstr /I "server-ip=" %properties% > %tmp_ip% >nul 2>&1
-findstr /I "server-port=" %properties% > %tmp_port% >nul 2>&1
+findstr /I "server-ip=" %properties% > %tmp_ip%
+findstr /I "server-port=" %properties% > %tmp_port%
+
+set /p "ip_unformatted=" < %tmp_ip% 
+set /p "port_unformatted= "< %tmp_port%
+if "%ip_unformatted%"=="server-ip=" set "ip_unformatted=server-ip=0.0.0.0"
+set ip=%ip_unformatted:~10,25%
+set port=%port_unformatted:~12,25%
+
 
 @rem ----------- server flags -------------
 set "akair=-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
 
 set "flags_basic=java -Xms512M -Xmx1000M -jar server.jar nogui"
-set "flags_akair=java -Xms4G -Xmx4G %akair%-jar server.jar nogui"
-set "flags_akair_custom=java -Xms%7 -Xmx%9 %akair% -jar !arg_10:~2,25!.jar nogui"
+set "flags_akair=java -Xms4G -Xmx4G %akair% -jar server.jar nogui"
+set "flags_akair_2=java -Xms8G -Xmx8G %akair% --add-modules=jdk.incubator.vector -jar server.jar nogui"
+set "flags_akair_custom_0=java -Xms%7 -Xmx%9 %akair% -jar server.jar nogui"
+set "flags_akair_custom_1=java -Xms%7 -Xmx%9 %akair% --add-modules=jdk.incubator.vector -jar server.jar nogui"
+
+
 
 goto :__main__
 
@@ -131,24 +140,30 @@ if "%~1"=="--name" (
                 echo %nI% Executing...
                 echo.
                 set "server_type=basics"
-                goto __execute__
+                goto :__update__
             ) else if "%~4"=="improved" (
                 echo %nI% Your server will spin up with %nRed% Improved flags... %nReset% 
                 echo %nI% %nYellow% -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 
                 echo %nI% %nYellow% -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 
                 echo %nI% %nYellow% -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20
                 echo %nI% %nYellow% -XX:InitiatingHeapOccupancyPercent=15 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true %nReset% 
-                echo %nS% java %nRed%-Xms4000M -Xmx4000M %nReset%-jar %nYellow%server.jar%nReset%
+                echo %nS% java %nRed%-Xms4000M -Xmx4000M %nReset% -jar %nYellow%server.jar%nReset%
                 echo.
                 set "server_type=improved"
-                goto __execute__
+                goto :__update__
             ) else if "%~4"=="advanced" (
                 echo %nI% Your server will spin up with %nRed%Adcenced flags... %nReset% 
+                echo %nS% java %nRed%-Xms8000M -Xmx8000M %nReset% --add-modules=jdk.incubator.vector -jar %nYellow%server.jar%nReset%
                 set "server_type=Advanced"
                 echo.
-                goto __flags+__                
+                goto :__update__                
             ) else if "%~4"=="custom" (
-                echo %nI% Your server will spin up with %nRed%Custom flags... %nReset% 
+                echo %nI% Your server will spin up with %nRed%Custom flags... %nReset%
+                set "server_type=Custom"
+                echo %nI% Your server will spin up with %nRed%Full customs flags -- Need specify Ram params... %nReset% 
+                echo %nS% java %nRed%--min "yourInput" --max "yourInput" %nReset% --winc ^| wionc -jar %nYellow%server.jar%nReset%
+                echo.
+                goto :__update__
             ) else (
                 echo.
                 echo %nW% Script ended, becouse you don't select type of flags %nReset%
@@ -168,8 +183,22 @@ if "%~5"=="--purpur" (
     set "latest_version=!purpur_version:~0,-3!"
     if "%~6"=="" (
         for /f "delims=" %%A in ('curl -ks https://api.purpurmc.org/v2/purpur/!latest_version! ^| jq-win64.exe .builds.latest') do set "build_string=%%~A"
+        curl -ks https://api.purpurmc.org/v2/purpur/!latest_version!/!build_string!/download -o server.jar
+        set "version=!latest_version!"
+        set "type=purpur"
+        call :__execute__ 
+    ) else if "%~6"=="latest" (
+        for /f "delims=" %%A in ('curl -ks https://api.purpurmc.org/v2/purpur/!latest_version! ^| jq-win64.exe .builds.latest') do set "build_string=%%~A"
+        curl -ks https://api.purpurmc.org/v2/purpur/!latest_version!/!build_string!/download -o server.jar
+        set "version=!latest_version!"
+        set "type=purpur"
+        call :__execute__
     ) else if not "!ver_string:%~6=!"=="!ver_string!" (
-        for /f "delims=" %%A in ('curl -ks https://api.purpurmc.org/v2/purpur/%~6! ^| jq-win64.exe .builds.latest') do set "build_string=%%~A"
+        for /f "delims=" %%A in ('curl -ks https://api.purpurmc.org/v2/purpur/%~6 ^| jq-win64.exe .builds.latest') do set "build_string=%%~A"
+        curl -ks https://api.purpurmc.org/v2/purpur/%~6/!build_string!/download -o server.jar
+        set "version=%~6"
+        set "type=purpur"
+        call :__execute__
     ) else (
         echo %nW%%nReset% This version %nYellow%%6%nReset% is not valid!
         exit /b
@@ -179,9 +208,23 @@ if "%~5"=="--purpur" (
     for %%A in (!ver_string!) do set "paper_version=%%~A"
     set "latest_version=!paper_version:~0,-3!"
     if "%~6"=="" (
-        for /f "delims=" %%A in ('curl -ks https://api.papermc.io/v2/projects/paper/!latest_version! ^| jq-win64.exe ".builds | last"') do set "build_string=%%~A"
+        for /f "delims=" %%A in ('curl -ks https://api.papermc.io/v2/projects/paper/versions/!latest_version! ^| jq-win64.exe ".builds | last"') do set "build_string=%%~A"
+        curl -ks https://api.papermc.io/v2/projects/paper/versions/!latest_version!/builds/!build_string!/downloads/paper-!latest_version!-!build_string!.jar -o server.jar
+        set "version=!latest_version!"
+        set "type=paper"
+        call :__execute__
+    ) else if "%~6"=="latest" (
+        for /f "delims=" %%A in ('curl -ks https://api.papermc.io/v2/projects/paper/versions/!latest_version! ^| jq-win64.exe ".builds | last"') do set "build_string=%%~A"
+        curl -ks https://api.papermc.io/v2/projects/paper/versions/!latest_version!/builds/!build_string!/downloads/paper-!latest_version!-!build_string!.jar -o server.jar
+        set "version=!latest_version!"
+        set "type=paper"
+        call :__execute_
     ) else if not "!ver_string:%~6=!"=="!ver_string!" (
         for /f "delims=" %%A in ('curl -ks https://api.papermc.io/v2/projects/paper/versions/%~6^| jq-win64.exe ".builds | last"') do set "build_string=%%~A"
+        curl -ks https://api.papermc.io/v2/projects/paper/versions/%~6/builds/!build_string!/downloads/paper-%~6-!build_string!.jar -o server.jar
+        set "version=%~6"
+        set "type=paper"
+        call :__execute__
     ) else (
         echo %nW%%nReset% This version %nYellow%%6%nReset% is not valid!
         exit /b
@@ -192,6 +235,16 @@ if "%~5"=="--purpur" (
     set "latest_version=!velocity_version!"
     if "%~6"=="" (
         for /f "delims=" %%A in ('curl -ks https://api.papermc.io/v2/projects/velocity/versions/!latest_version!/builds ^| jq-win64.exe ".builds | last | .build"') do set "build_string=%%~A"
+        curl -ks https://api.papermc.io/v2/projects/velocity/versions/!latest_version!/builds/!build_string!/downloads/velocity-!latest_version!-!build_string!.jar -o server.jar
+        set "version=!latest_version!"
+        set "type=velocity"
+        call :__execute__
+    ) else if "%~6"=="latest" (
+        for /f "delims=" %%A in ('curl -ks https://api.papermc.io/v2/projects/velocity/versions/!latest_version!/builds ^| jq-win64.exe ".builds | last | .build"') do set "build_string=%%~A"
+        curl -ks https://api.papermc.io/v2/projects/velocity/versions/!latest_version!/builds/!build_string!/downloads/velocity-!latest_version!-!build_string!.jar -o server.jar
+        set "version=!latest_version!"
+        set "type=velocity"
+        call :__execute__
     ) else (
         echo %nW%%nReset% You cannot insert any version on %nYellow%Velocity Project %nReset%!
         exit /b
@@ -202,8 +255,22 @@ if "%~5"=="--purpur" (
     set "latest_version=!velocity_version!"
     if "%~6"=="" (
         for /f "delims=" %%A in ('curl -ks https://api.papermc.io/v2/projects/waterfall/versions/!latest_version!/builds ^| jq-win64.exe ".builds | last | .build"') do set "build_string=%%~A"
+        curl -ks https://api.papermc.io/v2/projects/waterfall/versions/!latest_version!/builds/!build_string!/downloads/waterfall-!latest_version!-!build_string!.jar -o server.jar
+        set "version=!latest_version!"
+        set "type=waterfall"
+        call :__execute__
+    ) else if "%~6"=="latest" (
+        for /f "delims=" %%A in ('curl -ks https://api.papermc.io/v2/projects/waterfall/versions/!latest_version!/builds ^| jq-win64.exe ".builds | last | .build"') do set "build_string=%%~A"
+        curl -ks https://api.papermc.io/v2/projects/waterfall/versions/!latest_version!/builds/!build_string!/downloads/waterfall-!latest_version!-!build_string!.jar -o server.jar
+        set "version=!latest_version!"
+        set "type=waterfall"
+        call :__execute_
     ) else if not "!ver_string:%~6=!"=="!ver_string!" (
         for /f "delims=" %%A in ('curl -ks https://api.papermc.io/v2/projects/waterfall/versions/%~6/builds^| jq-win64.exe ".builds | last | .build"') do set "build_string=%%~A"
+        curl -ks https://api.papermc.io/v2/projects/waterfall/versions/%~6/builds/!build_string!/downloads/waterfall-%~6-!build_string!.jar -o server.jar
+        set "version=%~6"
+        set "type=waterfall"
+        call :__execute__
     ) else (
         echo %nW%%nReset% This version %nYellow%%6%nReset% is not valid!
         exit /b
@@ -215,19 +282,38 @@ if "%~5"=="--purpur" (
     exit /b 1  
 )
 
+goto :eof                 
 
+:__after__
+set "isRestarting=false"
+set "isCrashed=false"
 
-@rem echo %latest_version%
+>nul find "RestartCommand" logs/latest.log && set "isRestarting=true"
+>nul find "./start.sh' does not exist! Stopping server." logs/latest.log && set "isRestarting=true"
+>nul find "UncaughtExceptionHandler" logs/latest.log && set "isCrashed=true"
+>nul find "Exception:" logs/latest.log && set "isCrashed=true"
+>nul find "java.lang.NullPointerException" logs/latest.log && set "isCrashed=true"
+>nul find "java.lang.OutOfMemoryError" logs/latest.log && set "isCrashed=true"
 
-echo %
-echo %build_string%
-echo %server_type%
-
-goto :eof
-
-:__execute__
-if !server_type!==basics (
-    goto :__update__
+if "%isRestarting%"=="true" (
+    cls
+    echo.
+    echo %nS% Restarting your server %nReset%
+    echo %nS% Server Address %nBlueL%%ip%:%port% %nReset%
+    echo %nS% Public Server Address %nBlueL%%externalIP%:%port% %nReset%
+    timeout >nul 2>&1
+    call :__execute__
+) else if "%isCrashed%"=="true" (
+    cls
+    echo.
+    echo %nW% Your server crashed... Trying a restart %nReset%
+    timeout >nul 2>&1
+    call :__execute__    
+) else (
+    cls
+    echo.
+    echo %nW% You stopped server %nReset%
+    echo.   
 )
 
 goto :eof
@@ -235,7 +321,68 @@ goto :eof
 
 
 
+:__execute__
+del "logs\latest.log" >nul 2>&1
+echo eula=true>eula.txt
+
+if "%server_type%"=="basics" (
+    echo %nI% Type %nBlueL%%type% %nReset%Version %nYellow%%version%%nReset% Build %nGreenD%%build_string% %nReset%
+    echo.
+    echo.
+    %flags_basic%
+    call :__after__
+) else if "%server_type%"=="improved" (
+    echo %nI% Type %nBlueL%%type% %nReset%Version %nYellow%%version%%nReset% Build %nGreenD%%build_string% %nReset%
+    echo.
+    echo.
+    %flags_akair%
+    call :__after__
+) else if "%server_type%"=="Advanced" (
+    echo %nI% Type %nBlueL%%type% %nReset%Version %nYellow%%version%%nReset% Build %nGreenD%%build_string% %nReset%
+    echo.
+    echo.
+    %flags_akair_2%
+    call :__after__
+) else if "%server_type%"=="Custom" (
+    echo %nI% Type %nBlueL%%type% %nReset%Version %nYellow%%version%%nReset% Build %nGreenD%%build_string% %nReset%
+    echo.
+    if "!6"=="" (
+        echo %nW% You need to specify version %nBlueL%latest%nReset% ^| %nGreenD%Version %nReset%
+        echo.
+        exit /b 1
+    ) else if "%~6"=="latest" (
+        echo %nI% You alredy using %nYellow%Latest version %nReset%
+    ) else if not "%~6"=="" (
+        echo %nI% You alredy using %nYellow%%~6% version %nReset%
+    )
+    set "Counter=1"
+    for %%f in (%*) do (
+    set "arg_!Counter!=%%f"
+    set /A Counter+=1
+    )
+    if "%~8"=="" (
+        echo %nW% You need to specify %nYellow%value of Minimum %nReset%Ram via %nBlueL%e.g. --min 1G %nReset%
+        exit /b 1
+    )
+    if "!arg_10!"=="" (
+        echo %nW% You need to specify %nYellow%value of Maximum %nReset%Ram via %nBlueL%e.g. --max 10G %nReset%
+        exit /b 1
+    )
+    if "!arg_11!"=="--winc" (
+        echo %nS% You selected version with %nYellow% --add-modules=jdk.incubator.vector%nReset%
+        %flags_akair_custom_1%
+        call :__after__
+    )
+    if "!arg_11!"=="--wionc" (
+       echo  %nS% You selected version without %nRed% --add-modules=jdk.incubator.vector%nReset%
+        %flags_akair_custom_0%
+        call :__after__
+    ) else (
+        echo %nW% You need to specify --wmic or --wiomc etting for --add-modules=jdk.incubator.vector %nReset%
+    )
+) else (
+    exit /b 1
+)
 
 
-:__flags+__
-
+./new --name "Test" --flags custom --purpur latest | ver | --min 1000M --max 1000 --winc
